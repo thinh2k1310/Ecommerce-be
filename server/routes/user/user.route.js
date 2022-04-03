8047da46401b11ddefa17cc43427c9aefc295c5c
truong thinh <thinh2k1310@gmail.com>
2022-04-03 21:40:52 +0700

    Build get and update profile function of user module

--- server/routes/user/user.route.js
+++ server/routes/user/user.route.js
@@ -1,27 +1,15 @@
 const express = require('express');
 
+const userController = require('./user.controller');
+const auth = require('../../middleware/auth');
+
 const userRouter = express.Router();
 
-userRouter.get('/:userId', (req, res) => {
-    const userId = req.params.userId;
-  
-    User.findById(userId, (err, user) => {
-      res.status(200).json({
-        user: user
-      });
-    });
-  });
-  
-  userRouter.post('/:userId', (req, res) => {
-    const profile = req.body.profile;
-    let query = { _id: req.params.userId };
-  
-    User.updateOne(query, { profile: profile }, (err, user) => {
-      res.status(200).json({
-        success: 'updated',
-        user: user
-      });
-    });
-  });
+
+
+  userRouter.get('/', auth, userController.getUserProfile);
+
+  userRouter.put('/',auth, userController.updateUserProfile);
+
 
 module.exports = userRouter;
